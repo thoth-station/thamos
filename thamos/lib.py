f864b8cb432c62e0865a1ea0e191afcab7ed77a0
mayaCostantini <mcostant@redhat.com>
2022-01-05 21:54:31 +0100

    pre-commit

--- thamos/lib.py
+++ thamos/lib.py
@@ -1159,7 +1159,9 @@ def install(
 
 
 @with_api_client
-def list_thoth_container_images(api_client: ApiClient) -> typing.List[typing.Dict[str, Any]]:
+def list_thoth_container_images(
+    api_client: ApiClient,
+) -> typing.List[typing.Dict[str, Any]]:
     """Get available Thoth container images."""
     return (
         ContainerImagesApi(api_client)
